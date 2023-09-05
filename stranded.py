34479e301a6c6c565a21ab7b3b5fd084bcbe004b
dephillips98 <dephillips@gmail.com>
2023-09-05 17:06:06 -0500

    formated the description to look nice AB#153

--- stranded.py
+++ stranded.py
@@ -16,7 +16,13 @@ def main(stdscr):
     height, width = stdscr.getmaxyx()
     with open(os.path.abspath('./data/title.txt'), 'r', encoding='utf-8') as title_file:
         title_lines = title_file.readlines()
-    
+    with open("./data/description", "r") as plot:
+        plot = plot.read().splitlines()
+        plot_splice = []
+        splice_len = 50
+        for i in range(0, len(plot), splice_len):
+            plot_splice.append(plot[i:i+splice_len])
+        
     while True:
         # Clear the windows
         stdscr.clear()
@@ -26,21 +32,13 @@ def main(stdscr):
         for index, line in enumerate(title_lines):
             stdscr.addstr(index, (width - len(line)) // 2, f'{line}')
         stdscr.refresh()
+        for i, string_list in enumerate(plot_splice):
+            for j, string in enumerate(string_list):
+                stdscr.addstr(i + len(title_lines) + j, (width - len(string)) // 2, string)
 
-
-
-def display_strings(stdscr, plot):
-    # Initialize curses
-    curses.curs_set(0)  # Hide the cursor
-    stdscr.clear()
-    stdscr.refresh()
-    with open("./data/description", "r") as plot:
-        plot = plot.read().splitlines()
-    for i, string in enumerate(plot):
-        stdscr.addstr(i, 0, string.strip())
+            #stdscr.addstr(i + len(title_lines), (width - len(line)) // 2 , string.strip())
         stdscr.refresh()
         stdscr.getch()
-def main(stdscr):
-    curses.wrapper(display_strings, "strings.txt")
+
 if __name__ == '__main__':
     curses.wrapper(main)
