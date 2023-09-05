cb787bac2fc0fc9af5a2edf1601deb808588cca4
dephillips98 <dephillips@gmail.com>
2023-09-05 14:57:51 -0500

    was able to git the desciption to populate AB#153

--- stranded.py
+++ stranded.py
@@ -3,11 +3,20 @@
 
 
 #imports
+import curses
 
-
-def main(): pass
-
-with open("./data/description", "r") as plot:
-    plot = plot.read()
+def display_strings(stdscr, plot):
+    # Initialize curses
+    curses.curs_set(0)  # Hide the cursor
+    stdscr.clear()
+    stdscr.refresh()
+    with open("./data/description", "r") as plot:
+        plot = plot.read().splitlines()
+    for i, string in enumerate(plot):
+        stdscr.addstr(i, 0, string.strip())
+        stdscr.refresh()
+        stdscr.getch()
+def main(stdscr):
+    curses.wrapper(display_strings, "strings.txt")
 if __name__ == '__main__':
-    pass
+    curses.wrapper(main)
