a1230f3d4dc4b189e14136adf7d73506e91cba73
DarkWinged <James.Levi.Rogers@gmail.com>
2023-09-05 12:25:20 -0500

    Project setup (#1)
    * inital project setup AB#140
    
    * added missing directory AB#140

---
+++ app/entity.py
@@ -0,0 +1,4 @@
+#imports
+from game_object import game_object
+
+class entity(game_object): pass
---
+++ stranded.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# James L. Rogers | github.com/DarkWinged
+
+
+#imports
+
+
+def main(): pass
+
+
+if __name__ == '__main__':
+    pass
