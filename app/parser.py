0a6e0dc181703aefcc8974ac5cd111deff6c3467
DarkWinged <James.Levi.Rogers@gmail.com>
2023-09-06 14:51:02 -0500

    Text parser (#5)
    * started working on parser AB#157
    
    * updated parser logic AB#157

---
+++ app/parser.py
@@ -0,0 +1,73 @@
+
+#imports
+from os import path
+import yaml
+
+class Parser:
+    def __init__(self) -> None:
+        with open(path.abspath('./data/actions.yaml')) as actions_file:
+            actions = yaml.safe_load(actions_file)
+
+        self.action_list: list[str] = []
+        self.action_synonyms: dict[str, str] = {}
+
+        for action in list(actions.keys()):
+            self.action_list.append(action)
+            for synonym in actions[action]:
+                self.action_synonyms[synonym] = action
+
+        with open(path.abspath('./data/selectors.yaml')) as selectors_file:
+            selectors = yaml.safe_load(selectors_file)
+            
+        self.selector_list: list[str]
+        self.selector_synonyms: dict[str, str]
+       
+        for selector in list(selectors.keys()):
+            self.selector_list.append(selector)
+            for synonym in selectors[selector]:
+                self.selector_synonyms[synonym] = selector
+        
+        with open(path.abspath('./data/reservedkeywords.yaml')) as keywords_file:
+            keywords = yaml.safe_load(keywords_file)
+            
+        self.reserved_list: list[str]
+        self.reserved_synonyms: dict[str, str]
+       
+        for keywords in list(keywords.keys()):
+            self.reserved_list.append(keywords)
+            for synonym in keywords[keywords]:
+                self.reserved_synonyms[synonym] = keywords
+ 
+
+    def process_token(self, token: str) -> str:
+        if token in self.action_list:
+            return token
+        elif token in list(self.action_synonyms.keys()):
+            return self.action_synonyms[token]
+        elif token in self.selector_list:
+            return token
+        elif token in list(self.selector_synonyms.keys()):
+            return self.selector_synonyms[token]
+        elif token in self.reserved_list:
+            return token
+        elif token in list(self.reserved_synonyms.keys()):
+            return self.reserved_synonyms[token]
+        return token
+
+
+    def parse(self, user_input: str) -> None:
+        tokens = user_input.split()
+
+        command = []
+        for token in tokens:
+            processed = self.process_token(token)
+            if processed:
+                if processed in self.selector_list:
+                    if len(command) > 0 and command[0] not in self.selector_list:
+                        command.append(processed)
+                else:
+                    command.append(processed)
+
+        if command[0] not in self.action_list:
+            return []
+        return command
