566c4338aec569e0fe3be83680034f16ad87ec96
Salvador Sanchez <salvadorsanchez660@yahoo.com>
2023-09-13 14:05:01 -0500

    added character status AB#171

--- stranded.py
+++ stranded.py
@@ -240,6 +240,7 @@ def main(stdscr):
     game_state["current_scene"] = "title"
     game_state["current_location"] = 1
     game_state["location_name"] = ''
+   
 
     while True:
         if game_state["current_scene"] == "playing":
@@ -249,7 +250,7 @@ def main(stdscr):
             scenes[game_state["current_scene"]](stdscr, data[game_state["current_scene"]])
         if not input_text:
             input_text = ''
-        input_window.addstr(0, 0, f"{game_state.get('location_name', '')}>{input_text}")
+        input_window.addstr(0, 0, f"{game_objects['players'][0].state}@{game_state.get('location_name', '')}>{input_text}")
         if game_state.get('user_command', ''):
             stdscr.addstr(height - 2 , 0, ' '.join(game_state['user_command']))
         stdscr.refresh()
