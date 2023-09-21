d947701566ea6d6259ae7c2642f2252da6901801
Benjamin Osborn <osbjorn6352@gmail.com>
2023-09-21 16:06:55 -0500

    Added text to window. Works on some terminals.... AB#615

--- gui_scripts/gui.py
+++ gui_scripts/gui.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 root.title("Test window")
 
-label = Label(root, text="Test 101", bg="white")
+label = Label(root, text="Test 101", fg="spring green")
 label.pack()
 
 root.mainloop()
