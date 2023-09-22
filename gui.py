2c2217cde23f5a5b0aac97fecd18bae4667ece83
Alejandro Sanchez <33365377+Al3x-s@users.noreply.github.com>
2023-09-22 16:36:40 -0500

    bugfix AB#620

--- gui.py
+++ gui.py
@@ -1,10 +1,18 @@
 from tkinter import *
+import depos
+
 with open('./data/help.txt', 'r') as helpfile:
     help_array = helpfile.readlines()
     help_string = ''
 for line in help_array:
     help_string = help_string + line
     
+with open ('./data/title.txt') as title:
+    titlelines = title.readlines()
+    titlestring = ''
+for liens in titlelines:
+    titlestring = titlestring + liens
+
 def pop_up():
     layer = Toplevel(root)
     layer.geometry("450x700")
@@ -13,9 +21,13 @@ def pop_up():
     
 root = Tk()
 root.title("stranded_window")
-label = Label(root, text="STRANDED", fg="spring green")
+label = Label(root, text=depos.titlestringgg[0], fg="#000")
 label.pack()
-help_button = Button(root, text="HELP", bg="black", command=pop_up, height=2, width=10)
+help_button = Button(root, text="HELP", command=pop_up, height=2, width=10)
 help_button.pack(side=TOP)
-start_button = Button(root, text="START", bg="black", height=2, width=10)
+start_button = Button(root, text="START", height=2, width=10)
 start_button.pack()
+
+
+
+root.mainloop()
