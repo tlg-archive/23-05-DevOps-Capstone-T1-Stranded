433ba7e0497f4be8b9f91ab3f708569655263c3a
Alejandro Sanchez <33365377+Al3x-s@users.noreply.github.com>
2023-09-26 12:00:21 -0500

    added title screen AB#619

--- flask_remake/main.py
+++ flask_remake/main.py
@@ -7,65 +7,14 @@ app = Flask(__name__)
 current_room = "Room-one"
 space_suit_picked_up = False
 
-def load_data() -> dict[str, any]:
+def load_data():
     data = {}
     with open(
-        f"{'/'.join(os.path.abspath(__file__).split('/')[:-1])}/data/title.txt", 
+        f"{'/'.join(os.path.abspath(__file__).split('/')[:-1])}./data/title.txt", 
         'r',
         encoding="utf-8"
         ) as title_file:
-        data['title'] = title_file.readlines()
-
-    with open(
-        f"{'/'.join(os.path.abspath(__file__).split('/')[:-1])}/data/description.txt",
-        "r",
-        encoding="utf-8"
-        ) as plot:
-        plot = plot.read().splitlines()
-    plot_splice = []
-    splice_len = 50
-    for i in range(0, len(plot), splice_len):
-        plot_splice.append(plot[i:i+splice_len])
-    data['opening'] = plot_splice
-
-    with open(
-              f"{'/'.join(os.path.abspath(__file__).split('/')[:-1])}/data/help.txt",
-              "r",
-              encoding="utf-8"
-              ) as help_file:
-        data['help'] = help_file.read()
-    
-    with open(
-              f"{'/'.join(os.path.abspath(__file__).split('/')[:-1])}/data/map.txt",
-              "r",
-              encoding="utf-8"
-              ) as map_file:
-        data['map'] = map_file.read()
-
-    object_types = ['locations', 'items', 'transitions', 'players', 'containers', 'npcs', "journals", 'events']
-
-    for object_type in object_types:
-        with open(
-            f"{'/'.join(os.path.abspath(__file__).split('/')[:-1])}/data/{object_type}.json",
-            "r",
-            encoding="utf-8"
-            ) as loading:
-            data[object_type] = json.load(loading)
-    
-    with open(
-        f"{'/'.join(os.path.abspath(__file__).split('/')[:-1])}/data/victory.txt", 
-        'r',
-        encoding="utf-8"
-        ) as victory_file:
-        data['victory'] = victory_file.read()
-    
-    with open(
-        f"{'/'.join(os.path.abspath(__file__).split('/')[:-1])}/data/defeat.txt", 
-        'r',
-        encoding="utf-8"
-        ) as defeat_file:
-        data['defeat'] = defeat_file.read()
-
+        data['title'] = title_file.read()
     return data
 
 message = ""
@@ -77,18 +26,18 @@ rooms = {
         "item": ["item1", "item2" , "item3"]
     },
     "Room-Two": {
-        "description": "You are in Room 2. You see a space suit here.",
+        "description": "You are in Room 2",
         "next_room": "Room-Three",
-        "item": ["item1", "item2" , "item3"]
+        "item": ["Space Suit"]
     },
     "Room-Three": {
-        "description": "Congratulations! You are in Room 3 with the space suit. You've won!",
+        "description": "Congratulations! You are in the space pod with the space suit. You've won!",
         "next_room": None,
         "item": ["item1", "item2" , "item3"]
     },
 }
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/game", methods=["GET", "POST"])
 def game():
     global current_room
     global space_suit_picked_up
@@ -97,9 +46,7 @@ def game():
     if request.method == "POST":
         action = request.form['action'].lower()
         
-        
-        
-        
+
         if current_room == "Room-one":
             if action == "move room 2":
                 current_room = 'Room-Two'
@@ -112,6 +59,7 @@ def game():
         elif current_room == "Room-Two":
             if action == "pickup suit":
                 space_suit_picked_up = True
+                message = "Youve picked up the space suit"
             elif action == "look":
                 message = rooms[current_room]["item"]
             elif action == "move room 3":
@@ -127,6 +75,14 @@ def game():
             message = "you won!"
 
     return render_template("index.html", description=rooms[current_room], room=current_room, message=message)
+
+data = load_data()
+
+@app.route("/", methods=["GET", "POST"])
+def start():
+    if request.method == "POST":
+        return redirect(url_for("game"))
+    return render_template("start.html", title=data["title"])
 
 if __name__ == "__main__":
     app.run(debug=True)
